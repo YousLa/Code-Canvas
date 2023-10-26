0a68c6283d6581391f05e6b255a30f41f270fc60
Elena Rottin <elena.rottin@gmail.com>
2023-10-26 15:16:29 +0200

    elena

---
+++ pageccsdynamic.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemple de modification de contenu</title>
+    <link rel="stylesheet" href="./assets/css/animations.css">
+</head>
+<body>
+    <div class = "container"></div>
+    <div>
+        <img src="./images/spirovert.svg" alt="" class = "vert">
+    </div>
+    <div>         
+        <img src="./images/spirorange.svg" alt="" class = "orange">
+    </div>
+    <div>
+        <img src="./images/spirobleu.svg" alt="" class = "bleu">
+    </div>
+    <div>
+        <img src="./images/spirorose.svg" alt="" class = "rose">
+    </div>
+</body>
+</html>
+
