a25ce1fbabd5d6b6b2e8cf062884dd9330a76195
Hanane Chyabri <chyabri.hanane@gmail.com>
2023-10-26 13:54:51 +0200

    login with 2 styles

--- views/templates/header.php
+++ views/templates/header.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/css/resetCSS.css">
     <link rel="stylesheet" href="assets/css/globalCSS.css">
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/style2.css">
     <link rel="stylesheet" href="assets/css/app.css">
     <script src="assets/js/script.js" defer></script>
     <title>Code Canvas</title>
