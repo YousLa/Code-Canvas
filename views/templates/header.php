c8c4d44b3e37ae29759610a6a409a4f2e549651a
Hanane Chyabri <chyabri.hanane@gmail.com>
2023-10-26 09:08:05 +0200

    app

--- views/templates/header.php
+++ views/templates/header.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="assets/css/resetCSS.css">
+    <link rel="stylesheet" href="assets/css/app.css">
     <script src="assets/js/script.js" defer></script>
     <title>Code Canvas</title>
 
