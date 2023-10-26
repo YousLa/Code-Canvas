68f149e1b8f385f9c98415f6fcc2a1098fa5b3e4
Laurence Delforge <laurencedelforge@hotmail.com>
2023-10-26 12:38:24 +0200

    image page statique

---
+++ pagecssstatic.php
@@ -0,0 +1,37 @@
+<?php
+
+session_start();
+
+require_once 'views/templates/header.php';
+
+require_once 'views/templates/nav.php';
+
+?>
+
+<!-- HTML -->
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CSS static</title>
+    <link rel="stylesheet" href="./assets/css/cssstatic.css">
+
+<body>
+
+    <div class="style1 container">
+
+        <h1>CSS</h1>
+
+        <div class="forme1"></div>
+        <img src="./assets/img/" alt="">
+        <div class="forme2"></div>
+        <div class="forme3"></div>
+        <div class="forme4"></div>
+        <div class="forme5"></div>
+        <div class="forme6"></div>
+        <div class="forme7"></div>
+        <div class="forme8"></div>
+
+</body>
