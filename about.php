942d3199b7e541ead2d19fa478e1b7ce937e57de
Laurence Delforge <laurencedelforge@hotmail.com>
2023-10-27 11:34:31 +0200

    about ok

--- about.php
+++ about.php
@@ -9,14 +9,21 @@ require_once 'views/templates/nav.php';
 ?>
 
 <!-- html -->
+<html>
+    <body class='style1 app' >
+        <div class='about'>
 
-<p>Voici un site qui va vous permettre de comprendre ce dont on a besoin pour construire un site et quelle est la fonction de chaque langage.
-    L'HTML, le CSS, Javascript et PHP.</p>
-<p>Vous pouvez créer un compte et sauvegarder la page qui vous plait pour qu'elle s'affiche automatiquement lors de votre prochain passage sur notre site.</p>
-<p>Ce site a été réalisé en 3 jours lors de l'Hackhaton 2023 d'Interface3 à Bruxelles.</p>
-<p>Un chouette challenge de travail de groupe et de mise en pratique.</p>
-<p>Yousra (WEB 23), Hanane, Elena et Laurence (WAD23).</p>
-
-<button class="htmlButton language-button">
-            HTML
-</button>
+            <p class='texte'>Voici un site qui va vous permettre de comprendre ce dont on a besoin pour construire un site et quelle est la fonction de chaque langage.
+                L'HTML, le CSS, Javascript et PHP. 
+                <br>Vous pouvez créer un compte et sauvegarder la page qui vous plait pour qu'elle s'affiche automatiquement lors de votre prochain passage sur notre site. 
+                <br>Ce site a été réalisé en 3 jours lors de l'Hackhaton 2023 d'Interface3 à Bruxelles. Merci aux coachs! 
+                <br>Un chouette challenge de travail de groupe et de mise en pratique.</p>
+                <p class='texte'>Yousra (WEB 23), Hanane, Elena et Laurence (WAD23).</p>
+                
+                <a href="index.php"><button class="comic-button">
+                    retour
+                </button></a>
+                
+            </div>
+        </body>
+</html>
