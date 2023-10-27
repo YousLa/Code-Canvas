92273102c102b6b218278606a83686d5a8372cbc
Laurence Delforge <laurencedelforge@hotmail.com>
2023-10-27 10:59:06 +0200

    png + about

---
+++ about.php
@@ -0,0 +1,22 @@
+<?php
+
+session_start();
+
+require_once 'views/templates/header.php';
+
+require_once 'views/templates/nav.php';
+
+?>
+
+<!-- html -->
+
+<p>Voici un site qui va vous permettre de comprendre ce dont on a besoin pour construire un site et quelle est la fonction de chaque langage.
+    L'HTML, le CSS, Javascript et PHP.</p>
+<p>Vous pouvez créer un compte et sauvegarder la page qui vous plait pour qu'elle s'affiche automatiquement lors de votre prochain passage sur notre site.</p>
+<p>Ce site a été réalisé en 3 jours lors de l'Hackhaton 2023 d'Interface3 à Bruxelles.</p>
+<p>Un chouette challenge de travail de groupe et de mise en pratique.</p>
+<p>Yousra (WEB 23), Hanane, Elena et Laurence (WAD23).</p>
+
+<button class="htmlButton language-button">
+            HTML
+</button>
