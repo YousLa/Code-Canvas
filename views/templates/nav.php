dadea96b6168bc2f88667712098eab3208fbb9fb
unknown <chyabri.hanane@gmail.com>
2023-11-01 18:55:42 +0100

    final tous fonctionne

--- views/templates/nav.php
+++ views/templates/nav.php
@@ -4,21 +4,24 @@
     </div>
 
     <nav>
-    <form id="enregistrer" action="./controllers/session/likeVersionController.php" method="POST">
-        <input class="styleUser hidden-element" type="text" name="classe" value="">
+
         <ul>
+
             <li><a href="about.php"><img class="info" src="assets/img/icons/question-mark-question-svgrepo-com.svg" alt=""></a></li>
 
             <!-- Si la variable session existe c'est que l'utilisateur est connecté et on affiche le bouton de déconnexion -->
             <?php if (isset($_SESSION['id'])) : ?>
                 <li><button class="buttonLogout language-button"><a href="controllers/session/logoutController.php">Log out</a></button></li>
                 <!-- Si la varibale session n'existe pas on affiche le bouton de connexion -->
-                <li><button type="submit" class="boutonEnregistrer language-button">Enregistrer</button></li>
+                <li>
+                    <form id="enregistrer" action="./controllers/session/likeVersionController.php" method="POST">
+
+                <input class="styleUser hidden-element" type="text" name="classe" value="">
+                <button type="submit" class="boutonEnregistrer language-button">Enregistrer</button></form></li>
             <?php else : ?>
                 <li><button class="buttonLogin language-button"><a href="controllers/session/loginController.php">Login</a></button></li>
             <?php endif; ?>
         </ul>
-    </form>
+    
     </nav>
 </header>
-
